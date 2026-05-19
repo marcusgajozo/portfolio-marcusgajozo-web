8eb878d8f36c2522d5bd8102e7d0aee2122a6f53
Marcus Gajozo <marcusgajozo@gmail.com>
2026-05-19 14:13:21 -0400

    build(deps): remove eslint-plugin-tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-import tailwind from "eslint-plugin-tailwindcss";
 import unusedImports from "eslint-plugin-unused-imports";
 
 const eslintConfig = defineConfig([
@@ -13,10 +12,8 @@ const eslintConfig = defineConfig([
     plugins: {
       "simple-import-sort": simpleImportSort,
       "unused-imports": unusedImports,
-      tailwindcss: tailwind,
     },
     rules: {
-      "tailwindcss/classnames-order": "warn",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       "unused-imports/no-unused-imports": "error",
